c0013df08f36bb7cb8e492a9fb514df471afa1df
mmz123 <1163817801@qq.com>
2019-01-04 13:59:37 +0800

    commit

---
+++ src/bit/edu/controller/getAudio.java
@@ -0,0 +1,13 @@
+package bit.edu.controller;
+
+/**
+ * 界面端发出录音请求，java采集问题音频
+ * @author me
+ *
+ */
+
+public class getAudio {
+	
+	
+
+}
---
+++ src/bit/edu/view/Finder_main.java
@@ -0,0 +1,10 @@
+package bit.edu.view;
+
+public class Finder_main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
